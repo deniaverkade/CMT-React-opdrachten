1815c9019411aac8df72f85feae7cadff771775e
deniaverkade <9024406@student.zadkine.nl>
2025-10-10 13:07:57 +0200

    1.1
    Usestate loop naar BookList.jsx

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,27 +1,11 @@
 import { useState } from "react";
 
-const Book = () => {
-
-    const [books, setBooks] = useState([
-        {id:1, cover: "./public/images/book-1.png", title: "Harry Potter", author: "J.K. Rowling",},
-        {id:2, cover: "./public/images/book-2.png", title: "Fantasia VI", author: "Geronimo Stilton",},
-        {id:3, cover: "./public/images/book-3.png", title: "The Hunger Games", author: "Suzanne Collins",},
-    ])
-
-
-
+const Book = ({cover,title,author}) => {
     return ( 
         <>
-        
-            {books.map(book => (
-            <div key={book.id} className="book-container">
-            <img src={book.cover}/>
-            <h2>{book.title}</h2>
-            <h3>{book.author}</h3>
-            </div>
-            ))}
-
-       
+            <img src={cover}/>
+            <h2>{title}</h2>
+            <h3>{author}</h3>
         </>
      );
 }
--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Book from "./Book";
 
 const BookList = () => {
+
+const [books, setBooks] = useState([
+        {id:1, cover: "./public/images/book-1.png", title: "Harry Potter", author: "J.K. Rowling",},
+        {id:2, cover: "./public/images/book-2.png", title: "Fantasia VI", author: "Geronimo Stilton",},
+        {id:3, cover: "./public/images/book-3.png", title: "The Hunger Games", author: "Suzanne Collins",},
+    ])
+
   return ( 
     <>
     <div className="booklist-container">
-      <Book/>
+      {books.map(book => (
+            <div key={book.id} className="book-container">
+            <Book cover={book.cover} title={book.title} author={book.author}/>
+            </div>
+            ))}
+
+      
 
     </div>
     </>
