fa9f6a3b3f8921033312fb125a11a04d7ed791d1
deniaverkade <9024406@student.zadkine.nl>
2025-10-14 11:21:42 +0200

    header fix

--- opdracht-01-books/src/components/Header.jsx
+++ opdracht-01-books/src/components/Header.jsx
@@ -2,19 +2,13 @@ const Header = () => {
     return ( 
         <>
         <div className="navbar-container">
-            <div className="nav-content">
-                <div className="home">
-                    <p>Home</p>
-                </div>
-
-                <div className="contact">
-                    <p>Contact</p>
-                </div>
-
-                <div className="aboutus">
-                    <p>About us</p>
-                </div>
-            </div>
+          <nav>
+            <ul>
+                <li className="home">Home</li>
+                <li className="contact">Contact</li>
+                <li className="aboutus">About us</li>
+            </ul>
+          </nav>
         </div>
         </>
      );
