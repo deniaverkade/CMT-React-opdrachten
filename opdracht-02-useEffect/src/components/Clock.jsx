055715f3bfd9d8e059106b183bacb5964c3963c3
deniaverkade <9024406@student.zadkine.nl>
2025-10-15 12:49:09 +0200

    hier later mee verder

--- opdracht-02-useEffect/src/components/Clock.jsx
+++ opdracht-02-useEffect/src/components/Clock.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 const Time = () => {
     const [currentTime, setCurrentTime]  =  useState(new Date());
-    
 
+    useEffect(() => {
+        const intervalId =  setInterval(()  => {
+            const updateTime = currentTime.getTime;
+            
+
+        },1000);
+    })
 }
 
 const DateToday = () => {
