ddfc32d886814edfc766a5eaa6077f5e0788207c
deniaverkade <9024406@student.zadkine.nl>
2025-10-15 11:35:41 +0200

    1.3

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
 const Book = ({cover,title,author}) => {
+    const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+    const verhoogTeller =  () => {
+        setAantalKeerGelezen(aantalKeerGelezen+1)
+    }
     return ( 
         <>
             <img src={cover}/>
             <h2>{title}</h2>
             <h3>{author}</h3>
+            <button onClick={verhoogTeller}>Aantal keer gelezen: {aantalKeerGelezen}</button>
         </>
      );
 }
