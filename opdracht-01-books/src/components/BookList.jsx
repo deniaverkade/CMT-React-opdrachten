0fd10667c7d6996ae1e5f3a2b4f8f4f1a025993b
deniaverkade <9024406@student.zadkine.nl>
2025-12-02 11:56:22 +0100

    11

--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import Book from "./Book";
 import BookCounter from "./BookCounter";
+import booksData from "../data.js";
+
 
 const BookList = () => {
 
-const [books, setBooks] = useState([
-        {id:1, cover: "./public/images/book-1.png", title: "Harry Potter", author: "J.K. Rowling",},
-        {id:2, cover: "./public/images/book-2.png", title: "Fantasia VI", author: "Geronimo Stilton",},
-        {id:3, cover: "./public/images/book-3.png", title: "The Hunger Games", author: "Suzanne Collins",},
-    ])
+const [books, setBooks] = useState(booksData);
+const [searchInput, setSearchInput] =useState("");
+
+ const searchHandler = (e) => {
+        console.log(e.target.value);
+        let newSearch = e.target.value;
+        setSearchInput(newSearch);
+      
+        const filteredBooks = booksData.filter((book)=>
+        book.title.toLowerCase().includes(newSearch.toLowerCase())
+    )
+
+    setBooks(filteredBooks);
+    };
 
   return ( 
     <>
     <div>
       <BookCounter aantal={books.length}/>
+
     </div>
+     <div className="search">
+            <input type="text" placeholder="Zoek hier uw boek" name="search" onChange={searchHandler} value={searchInput}/>
+
+        </div>
     <div className="booklist-container">
+      
       {books.map(book => (
             <div key={book.id} className="book-container">
               
---
+++ opdracht-01-books/src/data.js
@@ -0,0 +1,24 @@
+const books = [
+    {
+        id: 1,
+        cover: "./public/images/book-1.png",
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+    },
+
+    {
+        id: 2,
+        cover: "./public/images/book-2.png",
+        title: "Fantasia VI",
+        author: "Geronimo Stilton",
+    },
+
+    {
+        id: 3,
+        cover: "./public/images/book-3.png",
+        title: "The Hunger Games",
+        author: "Suzanne Collins",
+    },
+];
+
+export default books;
