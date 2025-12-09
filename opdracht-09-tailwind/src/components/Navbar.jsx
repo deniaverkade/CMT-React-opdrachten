b3fac3d851e9bfde6c82f5d9f9f5915d09221dd2
deniaverkade <9024406@student.zadkine.nl>
2025-12-09 11:50:00 +0100

    tailwind

--- opdracht-09-tailwind/src/components/Navbar.jsx
+++ opdracht-09-tailwind/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 const Navbar = () => {
-    return ( <>
-    <nav class="flex flex-row list-none bg-stone-950 mb-20 h-10">
-        <li class="mr-5 text-white font-sans font-bold text-lg">Home</li>
-        <li class="mr-5 text-white font-sans font-bold text-lg">Contact</li>
-        <li class="mr-5 text-white font-sans font-bold text-lg">Boekingen</li>
-        <li class="mr-5 text-white font-sans font-bold text-lg">Over ons</li>
-    </nav>
-    </> );
+    return (<>
+        <nav class="flex flex-row list-none bg-indigo-300 dark:bg-neutral-950  mb-20 h-10">
+            <li class="mr-5 text-white font-sans dark:text-indigo-500 font-bold text-lg">Home</li>
+            <li class="mr-5 text-white font-sans dark:text-indigo-500 font-bold text-lg">Contact</li>
+            <li class="mr-5 text-white font-sans dark:text-indigo-500 font-bold text-lg">Boekingen</li>
+            <li class="mr-5 text-white font-sans dark:text-indigo-500 font-bold text-lg">Over ons</li>
+        </nav>
+    </>);
 }
- 
+
 export default Navbar;
