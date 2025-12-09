3bc60c8671846619f46b65c743faaf6bee217e0b
deniaverkade <9024406@student.zadkine.nl>
2025-12-09 11:41:31 +0100

    tailwind

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-center text-red-700">Opdracht 9 - Tailwind</h1>
+    <Navbar/>
+    <div class="flex-col w-150 ml-180">
+        <img class="h-10 w-10 absolute" src='/img/icon.svg' />
+        <h3 class="text-indigo-500 font-sans font-bold text-3xl ml-15 mt-4 relative"><span class="text-slate-900 dark:text-white" >Work</span>cation</h3>
+        <img class="h-70 w-110 rounded-2xl drop-shadow-2xl mt-6 mb-13" src='/img/woman.webp' />
+        <h1 class="text-slate-900 dark:text-white font-sans font-bold text-4xl w-100 mb-8">You can work from anywhere. <span class="text-indigo-500">Take advantage of it.</span></h1>
+        <p class="font-sans text-slate-900 text-xl mb-7 w-95 dark:text-white" >Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.</p>
+        <button class="bg-indigo-500 text-white h-12 w-50 font-sans font-bold rounded-md drop-shadow-md">BOOK YOUR ESCAPE</button>
+      </div>
+
     </>
   )
 }
---
+++ opdracht-09-tailwind/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( <>
+    <nav class="flex flex-row list-none bg-stone-950 mb-20 h-10">
+        <li class="mr-5 text-white font-sans font-bold text-lg">Home</li>
+        <li class="mr-5 text-white font-sans font-bold text-lg">Contact</li>
+        <li class="mr-5 text-white font-sans font-bold text-lg">Boekingen</li>
+        <li class="mr-5 text-white font-sans font-bold text-lg">Over ons</li>
+    </nav>
+    </> );
+}
+ 
+export default Navbar;
