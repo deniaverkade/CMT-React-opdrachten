87fe4b332b48019762eaba477a18db16b9a49300
deniaverkade <9024406@student.zadkine.nl>
2025-12-10 12:23:01 +0100

    Update HomePage.jsx

--- opdracht-06-zoo-app/src/components/HomePage.jsx
+++ opdracht-06-zoo-app/src/components/HomePage.jsx
@@ -7,33 +7,51 @@ import animalData from '../animals';
 const Home = () => {
     const navigate = useNavigate();
 
+
+    const [searchInput, setSearchInput] = useState("");
     const [animalList, setAnimalList] = useState(animalData);
 
 
     const goToDetail = (animalId) => {
         navigate(`/animals/${animalId}`);
     };
-     const animalMapping = animalList.map(ani =>(
-                
-                <Card
-                key={ani.id}
-                animal={ani}
-                onShowDetail={goToDetail}
-                /> 
-            
-            ))
+    const animalMapping = animalList.map(ani => (
 
-    return (  
-        
+        <Card
+            key={ani.id}
+            animal={ani}
+            onShowDetail={goToDetail}
+        />
+
+    ))
+
+
+
+     const searchHandler = (e) => {
+        console.log(e.target.value);
+        let newSearch = e.target.value;
+        setSearchInput(newSearch);
+      
+        const filteredAnimals = animalData.filter((animal)=>
+        animal.name.toLowerCase().includes(newSearch.toLowerCase())
+    )
+
+    setAnimalList(filteredAnimals);
+    };
+
+
+    return (
+
         <section>
             <h1 className="frontpageTitle">Zoo App</h1>
+            <input type="text" placeholder="Zoek hier uw dier" name="search" onChange={searchHandler} value={searchInput}/>
             <span className="container">
-            {animalMapping}
+                {animalMapping}
             </span>
         </section>
     );
 }
- 
+
 export default Home;
 
 
