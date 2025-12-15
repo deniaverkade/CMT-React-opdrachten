cb8f581945331cb0a7472be6227c42fdde224b94
deniaverkade <9024406@student.zadkine.nl>
2025-12-15 13:51:02 +0100

    qa

--- opdracht-13-flashcard-quiz/src/data.js
+++ opdracht-13-flashcard-quiz/src/data.js
@@ -1,103 +1,103 @@
 const questions = [
     {
         id: 1,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Nederland?",
+        answer:"Amsterdam"
     },
     {
         id: 2,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Duitsland?",
+        answer:"Berlijn"
     },
     {
         id: 3,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Tsjechië?",
+        answer:"Praag"
     },
     {
         id: 4,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Kroatië?",
+        answer:"Zagreb"
     },
     {
         id: 5,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Servië?",
+        answer:"Belgrado"
     },
     {
         id: 6,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van de Verenigde Staten?",
+        answer:"Washington D.C."
     },
     {
         id: 7,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Canada?",
+        answer:"Ottawa"
     },
     {
         id: 8,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Mexico?",
+        answer:"Mexico-Stad"
     },
     {
         id: 9,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Costa Rica?",
+        answer:" San Jóse"
     },
     {
         id: 10,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Cuba?",
+        answer:"Havana"
     },
     {
         id: 11,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Japan?",
+        answer:"Tokio"
     },
     {
         id: 12,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van China?",
+        answer:"Peking"
     },
     {
         id: 13,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Vietnam?",
+        answer:"Hanoi"
     },
     {
         id: 14,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Indonesie?",
+        answer:"Jakarta"
     },
     {
         id: 15,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van India?",
+        answer:"New Delhi"
     },
     {
         id: 16,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Egypte?",
+        answer:"Caïro"
     },
     {
         id: 17,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Marokko?",
+        answer:"Rabat"
     },
     {
         id: 18,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Senegal?",
+        answer:"Dakar"
     },
     {
         id: 19,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Zuid-Soedan?",
+        answer:"Juba"
     },
     {
         id: 20,
-        question: "",
-        answer:""
+        question: "Wat is de hoofdstad van Libië?",
+        answer:"Tripoli"
     },
 ];
 
